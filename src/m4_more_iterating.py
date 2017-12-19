fbef7c027436fc2922f342132dd5a82d1a022881
fusarocr <fusarocr@rose-hulman.edu>
2017-12-18 20:48:17 -0500

    count positive sines

--- src/m4_more_iterating.py
+++ src/m4_more_iterating.py
@@ -413,7 +413,7 @@ def count_items_bigger_than(numbers, threshold):
 def run_test_count_positive_sines():
     """ Tests the   count_positive_sines   function. """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # DONE: 6. Implement this TEST function.
     #   It TESTS the  count_positive_sines  function defined below.
     #   Include at least ** 1 ** ADDITIONAL test beyond what we supplied.
     #
@@ -509,7 +509,12 @@ def run_test_count_positive_sines():
     print('        actual:  ', actual)
 
     # TO DO 6 (continued):  Add your 1 ADDITIONAL test here:
-
+    # Test 11:
+    expected = 3
+    actual = count_positive_sines([9, 9, 4, 3, 6])
+    print()
+    print('Test 11 expected:', expected)
+    print('        actual:  ', actual)
 
 def count_positive_sines(numbers):
     """
@@ -535,10 +540,15 @@ def count_positive_sines(numbers):
       :type sequence: list or tuple (of numbers)
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # ------------------------------------------------------------------
 
+    count = 0
+    for k in range(len(numbers)):
+        if math.sin(numbers[k]) > 0:
+            count = count + 1
+    return count
 
 def run_test_sum_first_n():
     """ Tests the   sum_first_n   function. """
